22ef2cb72c5e7c25cae3c5494b0556908441f63d
alphaman8 <marcelo.sairaf@gmail.com>
2018-01-27 08:47:28 -0300

    -

--- sketch_sep23a/sketch_sep23a.ino
+++ sketch_sep23a/sketch_sep23a.ino
@@ -25,33 +25,18 @@ All text above, and the splash screen must be included in any redistribution
 #define OLED_RESET 4
 Adafruit_SSD1306 display(OLED_RESET);
 
-#define NUMFLAKES 10
-#define XPOS 0
-#define YPOS 1
-#define DELTAY 2
-
 #define DHTPIN 10
 #define DHTTYPE DHT11 
 
-#define LOGO16_GLCD_HEIGHT 16 
-#define LOGO16_GLCD_WIDTH  16 
-static const unsigned char PROGMEM logo16_glcd_bmp[] =
-{ B00000000, B11000000,
-  B00000001, B11000000,
-  B00000001, B11000000,
-  B00000011, B11100000,
-  B11110011, B11100000,
-  B11111110, B11111000,
-  B01111110, B11111111,
-  B00110011, B10011111,
-  B00011111, B11111100,
-  B00001101, B01110000,
-  B00011011, B10100000,
-  B00111111, B11100000,
-  B00111111, B11110000,
-  B01111100, B11110000,
-  B01110000, B01110000,
-  B00000000, B00110000 };
+#define humidity_width 20
+#define humidity_height 22
+static unsigned char humidity_bits[] = {
+   0x00, 0x00, 0x00, 0x80, 0x00, 0x00, 0xc0, 0x00, 0x00, 0xc0, 0x01, 0x00,
+   0xc0, 0x01, 0x00, 0xe0, 0x03, 0x00, 0xf0, 0x03, 0x00, 0xf0, 0x07, 0x00,
+   0xf8, 0x47, 0x00, 0xf8, 0x47, 0x00, 0xf8, 0xc5, 0x00, 0xf0, 0xe6, 0x00,
+   0xf0, 0xf3, 0x01, 0xc0, 0xf1, 0x01, 0x00, 0xf8, 0x03, 0x00, 0xf8, 0x03,
+   0x00, 0xfc, 0x07, 0x00, 0xfc, 0x06, 0x00, 0xf8, 0x06, 0x00, 0x78, 0x03,
+   0x00, 0xf0, 0x01, 0x00, 0x00, 0x00 };
 
 DHT dht(DHTPIN, DHTTYPE);
 
@@ -82,7 +67,7 @@ void setup()   {
   display.clearDisplay();
 
   // draw a bitmap icon and 'animate' movement
-  //testdrawbitmap(logo16_glcd_bmp, LOGO16_GLCD_HEIGHT, LOGO16_GLCD_WIDTH);
+  testdrawbitmap(humidity_bits, humidity_height, humidity_height);  
 }
 
 
